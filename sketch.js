e0fb046948227da795022d63f0cfc032639933f9
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-05-04 17:01:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function count()
+{
+  let numbers = [1,2,3,4,5,6,7,8,9];
+  let len = numbers.length;
+
+  for(let i = 0; i<len; i++)
+  {
+    console.log(numbers[i]);
+  }
+}
