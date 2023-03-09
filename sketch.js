5701fc1c8fafce5700cfbffc737650bd1f0d956a
Reena <reenachhablani96@gmail.com>
2023-03-10 00:42:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function count()
   let numbers = [1,2,3,4,5,6,7,8,9];
   let len = numbers.length;
 
-  for(let i = 0; i<len; i++)
+  for(let i = 1; i<len; i++)
   {
     console.log(numbers[i]);
   }
